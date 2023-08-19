812060839e7546d916c97a31fbbf2afa3e27cb72
cmlfnts <camiladejesusfontes@gmail.com>
2023-08-19 19:28:34 -0300

    sentença de código, chaves, blocos de cód e etc

---
+++ blocoDeCodigo/blocoDeCod_1.js
@@ -0,0 +1,24 @@
+{
+    console.log('Passo 01')
+    console.log('Passo 02')
+    console.log('Passo 03')
+    /*{
+        console.log('Passo 03')
+    }*/
+}
+
+{
+    console.log('Passo 04')
+    console.log('Passo 05')
+}
+
+//Bloco é o local onde agrupamos várias sentenças de código!!!!
+//Agrupar várias sentenças dentro de um único bloco, usamos chaves
+//Conseguimos criar um bloco de código com sentenças vazias, porém ele não executa o cód até o fim
+
+/*{
+    ;
+    ;
+    ;
+}*/
+console.log("Fim!!")
